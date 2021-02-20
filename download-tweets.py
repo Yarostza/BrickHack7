99e5ec9494439dd07ab436d79c5abfab57bd70e7
Zachary Yarost <yarostza@msu.edu>
2021-02-20 13:42:05 -0500

    added basic tweet pulling code

--- download-tweets.py
+++ download-tweets.py
@@ -1,1 +1,16 @@
-#This is my file
+import GetOldTweets3 as got
+import datetime
+
+user = "BarackObama"
+currentDate = datetime.date.today()
+
+startDate = str(currentDate - datetime.timedelta(days=365))
+endDate = str(currentDate)
+
+userTweets = got.manager.TweetCriteria().setUsername(user).setSince(startDate).\
+    setUntil(endDate).setMaxTweets(10).setEmoji("unicode")
+
+tweetList = got.manager.TweetManager.getTweets(userTweets)
+
+print(tweetList)
+#since 2015-09-10 --until 2015-09-12
