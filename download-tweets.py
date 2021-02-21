bc26e69e7af8e4cd4289c9b07df6c513fae1221d
Yanni Speron <yannisperon@gmail.com>
2021-02-20 16:25:11 -0800

    Removed need for file IO

--- download-tweets.py
+++ download-tweets.py
@@ -19,28 +19,20 @@ while True:
         if os.path.exists(fileName):
             os.remove(fileName)
 
+        tweets = []
         config = twint.Config()
         config.Username = username
         config.Limit = 10
-        config.Store_json = True
-        config.Output = fileName
         config.Hide_output = True
+        config.Store_object = True
+        config.Store_object_tweets_list = tweets
 
         twint.run.Search(config)
-
-        tweets = []
-        for line in open(fileName, 'r'):
-            tweets.append(line)
 
         tweetsCleaned = []
 
         for tweet in tweets:
-            try:
-                temp = p.clean(tweet.split("\"tweet\": \"")[1].split("\", \"language\"")[0])
-                temp2 = re.sub(r'[^\w\s]', '', temp)
-                tweetsCleaned.append(temp2)
-            except:
-                continue
+            tweetsCleaned.append(p.clean(tweet.tweet))
 
         def getSubjectivity(text):
             return tb.TextBlob(text).sentiment.subjectivity
@@ -48,16 +40,17 @@ while True:
         def getPolarity(text):
             return tb.TextBlob(text).sentiment.polarity
 
-        totalPolarity = 0.0
-        totalSubjectivity = 0.0
-        for tweet in tweetsCleaned:
-            totalPolarity += getPolarity(tweet)
-            totalSubjectivity += getSubjectivity(tweet)
-        totalPolarity /= len(tweetsCleaned)
-        totalSubjectivity /= len(tweetsCleaned)
-        print("Polarity (-1, 1): ", totalPolarity)
-        if totalPolarity <= -0.90:
-            print("    Extremely negative")
-        print("Subjective (0, 1): ", totalSubjectivity)
+        if len(tweetsCleaned) > 0:
+            totalPolarity = 0.0
+            totalSubjectivity = 0.0
+            for tweet in tweetsCleaned:
+                totalPolarity += getPolarity(tweet)
+                totalSubjectivity += getSubjectivity(tweet)
+            totalPolarity /= len(tweetsCleaned)
+            totalSubjectivity /= len(tweetsCleaned)
+            print("Polarity (-1, 1): ", totalPolarity)
+            if totalPolarity <= -0.90:
+                print("    Extremely negative")
+            print("Subjective (0, 1): ", totalSubjectivity)
     except:
         print("User could not be found")
